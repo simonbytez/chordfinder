f6bd732da742ac76c4cb884ac3215411b2a217cb
Jared Simon <jared.d.simon@gmail.com>
2022-04-02 09:18:19 -0500

    change size

--- src/salsa/components/Card.js
+++ src/salsa/components/Card.js
@@ -4,8 +4,8 @@ import {CardMedia} from '@mui/material';
 export default function Card(props) {
     return <>
         <MuiCard style={{
-            width: 125,
-            height: 125,
+            width: 100,
+            height: 100,
             margin: 12
         }}>
             <div onClick={props.onClick}>
@@ -14,8 +14,8 @@ export default function Card(props) {
                     image={props.image}
                     alt={props.alt}
                     style={{
-                        width: 125,
-                        height: 125,
+                        width: 100,
+                        height: 100,
                         opacity: props.selected ? 1.0 : 0.5,
                         objectFit: 'contain',
                         cursor: 'pointer'
